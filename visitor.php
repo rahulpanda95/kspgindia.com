d971453f746560c85f407fbeaf81e1498a87672b
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2018-12-23 14:07:53 +0530

    visitor3

--- visitor.php
+++ visitor.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "localhost";
+$servername = "rahulpanda.in:3306";
 $username = "rahulpan_admin";
 $password = "rp1995";
 $dbname = "rahulpan_kspg";
@@ -11,21 +11,21 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$date = $_POST['pdate'];
-$time = $_POST['ptime'];
-$ip = $_POST['visitor_ip'];
-$isp = $_POST['visitor_isp'];
-$org = $_POST['visitor_org'];
-$longitude = $_POST['visitor_longitude'];
-$latitude = $_POST['visitor_latitude'];
-$country_code = $_POST['visitor_country_code'];
-$country_name = $_POST['visitor_country_name'];
-$continent_code = $_POST['visitor_continent_code'];
-$currency_code = $_POST['visitor_currency_code'];
-$currency_name = $_POST['visitor_currency_name'];
+$date = $_GET['pdate'];
+$time = $_GET['ptime'];
+$ip = $_GET['visitor_ip'];
+$isp = $_GET['visitor_isp'];
+$org = $_GET['visitor_org'];
+$longitude = $_GET['visitor_longitude'];
+$latitude = $_GET['visitor_latitude'];
+$country_code = $_GET['visitor_country_code'];
+$country_name = $_GET['visitor_country_name'];
+$continent_code = $_GET['visitor_continent_code'];
+$currency_code = $_GET['visitor_currency_code'];
+$currency_name = $_GET['visitor_currency_name'];
 
 $sql = "INSERT INTO rahulpan_kspg.visitors (visit_date, visit_time, ip, isp, org, longitude, latitude, country_code, country_name, continent_code, currency_code, currency_name)
-VALUES ('$date','$time','$ip','$isp','$org','$longitude','$latitude','$currency_code','$country_name','$continent_code','$currency_code','$currency_name')";
+VALUES ('$date','$time','$ip','$isp','$org','$longitude','$latitude','$country_code','$country_name','$continent_code','$currency_code','$currency_name')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
@@ -34,5 +34,6 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
+echo "<script>window.close();</script>";
 ?>
 
