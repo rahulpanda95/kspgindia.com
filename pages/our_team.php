42a53f39f144e8af0da486debb2eb94652bf8c2b
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-12 18:49:22 +0530

    Index Change 61

--- pages/our_team.php
+++ pages/our_team.php
@@ -8,19 +8,7 @@
 ?>
 <html lang="en">
 <body>
-<div class="row">
-    <div class="column">
-        <div class="card">
-            <img src="img1.jpg" alt="Jane" style="width:100%">
-            <div class="container">
-                <h2>Jane Doe</h2>
-                <p class="title">CEO &amp; Founder</p>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>example@example.com</p>
-                <p><button class="button">Contact</button></p>
-            </div>
-        </div>
-    </div>
+
 
 </body>
 
