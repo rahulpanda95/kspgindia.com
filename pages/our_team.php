3858b1898982d73881777d468d14ef0fbda6ea74
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-13 00:05:22 +0530

    Index Change 75

--- pages/our_team.php
+++ pages/our_team.php
@@ -9,7 +9,20 @@
 <html lang="en">
 <body>
 
-<h4 align="center">Page under Construction</h4>
+<div class="post-thumb">
+    <a href="https://www.google.com/maps/place/Utkal+Ashram+Rd,+Brahmapur,+Odisha+760001/@19.3094461,84.7938961,17z/data=!3m1!4b1!4m5!3m4!1s0x3a3d5074befaf47f:0xf5861c1b6cdfbff4!8m2!3d19.308983!4d84.7955136" target="_blank"><img src="img/bg-img/MAP.png" alt="KSPG Publication"></a>
+</div>
+<!-- Post Data -->
+<div class="post-data">
+    <a href="#" class="post-catagory" style="background-color: #e1395a;">KSPG India Headquarters</a>
+    <a href="https://www.google.com/maps/place/Utkal+Ashram+Rd,+Brahmapur,+Odisha+760001/@19.3094461,84.7938961,17z/data=!3m1!4b1!4m5!3m4!1s0x3a3d5074befaf47f:0xf5861c1b6cdfbff4!8m2!3d19.308983!4d84.7955136" target="_blank" class="post-title">
+        <p><strong>13404, 26 Utkal Ashram Road<br>Subarao Square<br>Berhampur, Odisha.<br>PIN: 760001</strong></p>
+    </a>
+    <div class="post-meta">
+        <p class="post-author">Mobile: <a href="mailto:drdebashisha@kspgindia.com">9437617602</a></p>
+        <p class="post-date">Dr.Debashisha Panda, Co-Founder, KSPG India</p>
+    </div>
+</div>
 
 </body>
 
