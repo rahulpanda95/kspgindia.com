71917140a42c030b4d5fd5644a7d4274278bf11b
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-12 17:51:09 +0530

    Index Change 46

--- pages/reach_us.php
+++ pages/reach_us.php
@@ -56,12 +56,12 @@
                                 <div class="single-blog-post style-3 style-5 d-flex align-items-center mb-50">
                                     <!-- Post Thumb -->
                                     <div class="post-thumb">
-                                        <a href="#"><img src="img/bg-img/MAP.png" alt="KSPG Publication"></a>
+                                        <a href="https://www.google.com/maps/place/Utkal+Ashram+Rd,+Brahmapur,+Odisha+760001/@19.3094461,84.7938961,17z/data=!3m1!4b1!4m5!3m4!1s0x3a3d5074befaf47f:0xf5861c1b6cdfbff4!8m2!3d19.308983!4d84.7955136"><img src="img/bg-img/MAP.png" alt="KSPG Publication"></a>
                                     </div>
                                     <!-- Post Data -->
                                     <div class="post-data">
                                         <a href="#" class="post-catagory" style="background-color: #e1395a;">KSPG India Headquarters</a>
-                                        <a href="#" class="post-title">
+                                        <a href="https://www.google.com/maps/place/Utkal+Ashram+Rd,+Brahmapur,+Odisha+760001/@19.3094461,84.7938961,17z/data=!3m1!4b1!4m5!3m4!1s0x3a3d5074befaf47f:0xf5861c1b6cdfbff4!8m2!3d19.308983!4d84.7955136" class="post-title">
                                             <p><strong>13404, 26 Utkal Ashram Road<br>Subarao Square<br>Berhampur, Odisha.<br>PIN: 760001</strong></p>
                                         </a>
                                         <div class="post-meta">
