2cbeab8e280201514156ab82c1eda50d6bb2ec86
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-13 00:10:29 +0530

    Index Change 76

--- pages/our_team.php
+++ pages/our_team.php
@@ -13,7 +13,7 @@
     <a href="#" target="_blank"><img src="img/profile/sarita.png" alt="Sarita Panda" height="400" width="400"></a>
 </div>
 <!-- Post Data -->
-<div class="post-data">
+<div class="post-data" align="center">
     <a href="#" class="post-catagory" style="background-color: #e1395a;">KSPG India Headquarters</a>
     <a href="https://www.google.com/maps/place/Utkal+Ashram+Rd,+Brahmapur,+Odisha+760001/@19.3094461,84.7938961,17z/data=!3m1!4b1!4m5!3m4!1s0x3a3d5074befaf47f:0xf5861c1b6cdfbff4!8m2!3d19.308983!4d84.7955136" target="_blank" class="post-title">
         <p><strong>13404, 26 Utkal Ashram Road<br>Subarao Square<br>Berhampur, Odisha.<br>PIN: 760001</strong></p>
