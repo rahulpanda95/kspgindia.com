33806cd3e4d8703709bcf45ffa33881b165889ac
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-12 21:36:15 +0530

    Index Change 71

--- pages/ks_media_group.php
+++ pages/ks_media_group.php
@@ -8,8 +8,57 @@
 ?>
 <html lang="en">
 <body>
+<!-- ##### Blog Area Start ##### -->
+<div class="blog-area section-padding-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-8">
+                <div class="blog-posts-area">
 
-<h4 align="center">Page under Construction</h4>
+                    <!-- Related Articles Area -->
+                    <div class="related-articles-">
+                        <!--                        <h5 class="mb-70">Publish your Books, Articles, Journals, etc. with Us</h5>-->
+                        <div class="row">
+                            <!-- Single Post -->
+                            <div class="col-12">
+                                <div class="single-blog-post style-3 style-5 d-flex align-items-center mb-50">
+                                    <!-- Post Thumb -->
+                                    <div class="post-thumb">
+                                        <a href="#"><img src="img/core-img/kspg_media_group_with_border.png" alt="KSPG Publication"></a>
+                                    </div>
+                                    <!-- Post Data -->
+                                    <div class="post-data">
+                                        <a href="#" class="post-catagory" style="background-color: #e1395a;">PUBLICATION</a>
+                                        <a href="#" class="post-title">
+                                            <h6>We welcome talented Authors to publish their piece of art with us.</h6>
+                                        </a>
+                                        <div class="post-meta">
+                                            <p class="post-author">To know more, write us at:<a href="mailto:drdebashisha@kspgindia.com">drdebashisha@kspgindia.com</a></p>
+                                            <p class="post-date">Dr.Debashisha Panda, Co-Founder, KSPG India</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+<!-- ##### Blog Area End ##### -->
 
 </body>
 
