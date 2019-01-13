c101ab158488a28a57095b777bad69a904a98dd3
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-13 09:03:31 +0530

    13jan 3

--- pages/reach_us.php
+++ pages/reach_us.php
@@ -54,8 +54,8 @@
     <form action="" method="post">
         <p style="color: #e1395a"><strong>Name: <input type="text" name="name"></strong></p>
         <p style="color: #e1395a"><strong>Email: </strong><input type="text" name="email"></p>
-        <p style="color: #e1395a"><strong><u>Message</u></strong><br><textarea rows="10" name="message" cols="50"></textarea></p>
-        <input type="submit" name="submit" value="Submit" style="color: #3189e1">
+        <p style="color: #e1395a"><strong><u>Message</u></strong><br><textarea rows="10" name="message" cols="50" style="border: solid"></textarea></p>
+        <input type="submit" name="submit" value="Submit" style="background-color: #3189e1; color: white">
     </form>
     <br>
 </div>
