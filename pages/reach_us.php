65b0a629c051c9fa07fccf7c0a88d80a412eb908
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-13 09:08:06 +0530

    13jan 4

--- pages/reach_us.php
+++ pages/reach_us.php
@@ -1,10 +1,20 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: rahul
- * Date: 2019-01-12
- * Time: 11:35
- */
+if(isset($_POST['submit'])){
+    $to = "rahul@kspgindia.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name ." wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+}
 ?>
 <html lang="en">
 <body>
@@ -55,7 +65,7 @@
         <p style="color: #e1395a"><strong>Name: <input type="text" name="name"></strong></p>
         <p style="color: #e1395a"><strong>Email: </strong><input type="text" name="email"></p>
         <p style="color: #e1395a"><strong><u>Message</u></strong><br><textarea rows="10" name="message" cols="50" style="border: solid"></textarea></p>
-        <input type="submit" name="submit" value="Submit" style="background-color: #3189e1; color: white">
+        <input type="submit" name="submit" value="Submit" style="color: #3189e1">
     </form>
     <br>
 </div>
