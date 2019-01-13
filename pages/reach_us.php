c405d7319932d5035a5a25223bb3ce86c53d1a9e
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-13 09:00:52 +0530

    13jan 3

--- pages/reach_us.php
+++ pages/reach_us.php
@@ -14,9 +14,6 @@
         <div class="row">
             <div class="col-12 col-lg-8">
                 <div class="blog-posts-area">
-
-
-
                     <!-- Related Articles Area -->
                     <div class="related-articles-">
 
@@ -42,37 +39,6 @@
 
                                 </div>
                             </div>
-
-
-                        </div>
-                    </div>
-
-
-
-                    <div class="post-a-comment-area">
-                        <!-- <h4 class="mb-70">Leave a comment</h4> -->
-
-                        <!-- Reply Form -->
-                        <div class="contact-form-area">
-                            <form action="#" method="post">
-                                <div class="row">
-                                    <!-- <div class="col-12 col-lg-6">
-                                        <input type="text" class="form-control" id="name" placeholder="Name*">
-                                    </div>
-                                    <div class="col-12 col-lg-6">
-                                        <input type="email" class="form-control" id="email" placeholder="Email*">
-                                    </div>
-                                    <div class="col-12">
-                                        <input type="text" class="form-control" id="subject" placeholder="Website">
-                                    </div>
-                                    <div class="col-12">
-                                        <textarea name="message" class="form-control" id="message" cols="30" rows="10" placeholder="Message"></textarea>
-                                    </div>
-                                    <div class="col-12">
-                                        <button class="btn viral-btn mt-30" type="submit">Submit Comment</button>
-                                    </div> -->
-                                </div>
-                            </form>
                         </div>
                     </div>
                 </div>
