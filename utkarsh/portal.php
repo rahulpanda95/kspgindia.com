04812160d202973a1f2357c3a4ee7ed82518c65c
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-18 04:32:35 +0530

    add utkarsh new

--- utkarsh/portal.php
+++ utkarsh/portal.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["email"])) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Title Page</title>
+    <title>Utkarsh - KSPG India</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
