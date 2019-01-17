15436e59e2fb8972152f275c89e1439adc7400cc
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-18 04:36:08 +0530

    add utkarsh new

--- utkarsh/portal.php
+++ utkarsh/portal.php
@@ -19,8 +19,8 @@ if (!isset($_SESSION["email"])) {
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <!--<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>-->
-    <!--<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>-->
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
     <!-- Custom styles for this template -->
@@ -46,10 +46,10 @@ if (!isset($_SESSION["email"])) {
 
 
 <!-- jQuery -->
-<!--<script src="//code.jquery.com/jquery.js"></script>-->
+<script src="//code.jquery.com/jquery.js"></script>
 <!-- Bootstrap JavaScript -->
-<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>-->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<!--<script src="Hello World"></script>-->
+<script src="Hello World"></script>
 </body>
 </html>
