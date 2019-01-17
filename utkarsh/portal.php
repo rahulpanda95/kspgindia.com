ac039492bdcd266433b147911fd490b042849dda
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-18 04:58:57 +0530

    add utkarsh new

--- utkarsh/portal.php
+++ utkarsh/portal.php
@@ -52,7 +52,7 @@ if (!isset($_SESSION["email"])) {
                 <nav class="classy-navbar justify-content-between" id="viralnewsNav">
 
                     <!-- Logo -->
-                    <a class="nav-brand" href="https://kspgindia.com/"><img src="img/core-img/kspg_india_logo.png" alt="KSPG India Logo" height="100" width="100"></a>
+                    <a class="nav-brand" href="https://kspgindia.com/"><img src="../img/core-img/kspg_india_logo.png" alt="KSPG India Logo" height="100" width="100"></a>
 
                     <!-- Navbar Toggler -->
                     <div class="classy-navbar-toggler">
