8b3f435c4a25fdcd1bf6a7c5ff7901a93bea6d15
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-17 19:49:52 +0530

    add utkarsh new

--- utkarsh/portal.php
+++ utkarsh/portal.php
@@ -16,22 +16,22 @@ if (!isset($_SESSION["email"])) {
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-<!--    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->-->
-<!--    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->-->
-<!--    <!--[if lt IE 9]>-->
-<!--    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>-->
-<!--    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>-->
-<!--    <![endif]-->-->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <!--<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>-->
+    <!--<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>-->
+    <![endif]-->
 </head>
 <body>
 <h1 class="text-center">Welcome <?php echo $_SESSION["email"]; ?></h1>
 <p class="text-center"><a href="logout.php" >Click Here to logout</a></p>
 
 <!-- jQuery -->
-<script src="//code.jquery.com/jquery.js"></script>
+<!--<script src="//code.jquery.com/jquery.js"></script>-->
 <!-- Bootstrap JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>-->
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="Hello World"></script>
+<!--<script src="Hello World"></script>-->
 </body>
 </html>
