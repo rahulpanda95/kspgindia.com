b9e7f69651550c207c94c0a2b14ea53404178936
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-19 09:54:22 +0530

    EDIT HISTORY 9

--- utkarsh/portal.php
+++ utkarsh/portal.php
@@ -22,8 +22,6 @@ if (!isset($_SESSION["email"])) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
 
     <!-- Custom styles for this template -->
@@ -42,54 +40,7 @@ if (!isset($_SESSION["email"])) {
 </head>
 <body>
 
-<!--<header class="header-area">-->
-<!--    <div class="viral-news-main-menu" id="stickyMenu">-->
-<!--        <div class="classy-nav-container breakpoint-off">-->
-<!--            <div class="container">-->
-                <!-- Menu -->
-<!--                <nav class="classy-navbar justify-content-between" id="viralnewsNav">-->
-<!---->
-                    <!-- Logo -->
-<!--                    <a class="nav-brand" href="#"><img src="../img/core-img/kspg_india_logo.png" alt="KSPG India Logo" height="100" width="100"></a>-->
-<!---->
-                    <!-- Navbar Toggler -->
-<!--                    <div class="classy-navbar-toggler">-->
-<!--                        <span class="navbarToggler"><span></span><span></span><span></span></span>-->
-<!--                    </div>-->
-<!---->
-                    <!-- Menu -->
-<!--                    <div class="classy-menu">-->
-<!---->
-                        <!-- close btn -->
-<!--                        <div class="classycloseIcon">-->
-<!--                            <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>-->
-<!--                        </div>-->
-<!---->
-                        <!-- Nav Start -->
-<!--                        <div class="classynav">-->
-<!--                            <ul>-->
-<!--                                <li class="active"><a href="#">Welcome - --><?php //echo $_SESSION["email"]; ?><!--</a></li>-->
-<!--                            </ul>-->
-<!---->
-<!---->
-                            <!-- Video Post Button -->
-<!--                            <div class="add-post-button">-->
-<!--                                <a href="logout.php" class="btn add-post-btn">Log Out</a>-->
-<!--                            </div>-->
-<!---->
-<!--                        </div>-->
-                        <!-- Nav End -->
-<!--                    </div>-->
-<!--                </nav>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</header>-->
-<!-- ##### Header Area End ##### -->
-<!--<div class="h-divider">-->
-<!--<button onclick="window.location.href='logout.php'" name="logout" >Log Out</button>-->
-<!--<h1 class="text-center">Welcome --><?php //echo $_SESSION["email"]; ?><!--</h1>-->
-<!--<p class="text-center"><a href="logout.php" >Click Here to logout</a></p>-->
+<!--NavBar Starts-->
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
@@ -134,104 +85,65 @@ if (!isset($_SESSION["email"])) {
     <h3>Navbar With Dropdown</h3>
     <p>This example adds a dropdown menu for the "Page 1" button in the navigation bar.</p>
 </div>
+<!--NavBar Ends-->
 
-
-<footer id="footer" class="clearfix">
-    <div id="footer-widgets">
-
-        <div class="container">
-
-            <div id="footer-wrapper">
-
-                <div class="row">
-                    <div class="col-sm-6 col-md-2">
-                        <div id="meta-3" class="widget widgetFooter widget_meta">
-                            <h4 class="widgettitle">Importent Page :</h4>
-                            <ul>
-                                <li><a href="#"><i class="fa fa-home fa-fw"></i> Home</a></li>
-                                <li><a href="privacypolicy.php"><i class="fa fa-link"></i>   Privacy Policy</a></li>
-                                <li><a href="#"><i class="fa fa-envelope fa-fw"></i> Contact Us</a></li>
-                            </ul>
-                        </div>      </div> <!-- end widget1 -->
-
-                    <div class="col-sm-6 col-md-2">
-                        <div id="recent-posts-3" class="widget widgetFooter widget_recent_entries">
-                            <h4 class="widgettitle">Our social media :</h4>
-                            <ul>
-                                <li>
-                                    <a href="https://www.facebook.com/mr.dauod" target="_blank"><i class="fa fa-facebook"></i> Facebook</a>
-                                </li>
-                                <li>
-                                    <a href="https://www.twitter.com/ayatquranCom" target="_blank"><i class="fa fa-twitter"></i> Twitter</a>
-                                </li>
-                                <li>
-                                    <a href="http://www.youtube.com/user/sonsofye" target="_blank"><i class="fa fa-youtube"></i> Youtube</a>
-                                </li>
-                            </ul>
-                        </div>            </div> <!-- end widget1 -->
-
-                    <div class="col-sm-6 col-md-4">
-                        <div id="meta-4" class="widget widgetFooter widget_meta">
-                            <h4 class="widgettitle">Subscribe :</h4>
-
-                            <div class="form-group">
-                                <div class="input-group">
-                                    <span class="input-group-addon">E-mail :</span>
-                                    <input class="form-control" placeholder="Write your email .." type="text">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="input-group">
-                                    <span class="input-group-addon">Phone  :</span>
-                                    <input class="form-control" placeholder="+96777000000" type="text">
-                                    <span class="input-group-btn">
-                    <button class="btn btn-primary" type="button">Subscribe !</button>
-                  </span>
-                                </div>
-                            </div>
-
-
-                        </div>
-                    </div> <!-- end widget1 -->
-
-                    <div class="col-sm-6 col-md-4">
-                        <div id="search-3" class="widget widgetFooter widget_search"><h4 class="widgettitle">Search in Faploo :</h4>
-
-                            <div class="form-group">
-                                <label class="control-label">Write what you want to search for it  :</label>
-                                <div class="input-group">
-                                    <span class="input-group-addon">Keyword :</span>
-                                    <input class="form-control" placeholder="Keyword for search ..." type="text">
-                                    <span class="input-group-btn">
-                                    <button class="btn btn-primary" type="button">Search !</button>
-                                  </span>
-                                </div>
-                            </div>
-
-                        </div>            </div> <!-- end widget1 -->
-
-                </div> <!-- end .row -->
-
-            </div> <!-- end #footer-wrapper -->
-
-        </div> <!-- end .container -->
-    </div> <!-- end #footer-widgets -->
-
-    <div id="sub-floor">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 copyright">
-                    Copy right for Faploo © 2015
-                </div>
-                <div class="col-md-4 col-md-offset-4 attribution">
-                    Developer by  <a target="_blank" href="#">Faploo team</a> .
-                </div>
-            </div> <!-- end .row -->
+<!-- Footer -->
+<section id="footer">
+    <div class="container-fluid">
+        <div class="row text-center text-xs-center text-sm-left text-md-left">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h5>Quick links</h5>
+                <ul class="list-unstyled quick-links">
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h5>Quick links</h5>
+                <ul class="list-unstyled quick-links">
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h5>Quick links</h5>
+                <ul class="list-unstyled quick-links">
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+                    <li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+                <ul class="list-unstyled list-inline social text-center">
+                    <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
+                    <li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                </ul>
+            </div>
+            </hr>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                <p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
+                <p class="h6">&copy All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+            </div>
+            </hr>
         </div>
     </div>
-
-</footer>
+</section>
+<!-- ./Footer -->
 
 </body>
 </html>
