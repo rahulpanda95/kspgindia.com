43971238bd722544d1253e8788426eac19876b39
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-19 09:52:15 +0530

    EDIT HISTORY 9

--- utkarsh/portal.php
+++ utkarsh/portal.php
@@ -22,6 +22,8 @@ if (!isset($_SESSION["email"])) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
 
     <!-- Custom styles for this template -->
