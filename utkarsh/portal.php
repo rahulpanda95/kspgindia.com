68c5b2170b034ce864315ae5ce5c4408907ddf54
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-19 09:42:38 +0530

    EDIT HISTORY 9

--- utkarsh/portal.php
+++ utkarsh/portal.php
@@ -22,12 +22,7 @@ if (!isset($_SESSION["email"])) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <!--<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>-->
-    <!--<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>-->
-    <![endif]-->
+
 
     <!-- Custom styles for this template -->
     <style type="text/css">
@@ -133,21 +128,15 @@ if (!isset($_SESSION["email"])) {
         </div>
     </nav>
 
-    <div class="container">
-        <h3>Navbar With Dropdown</h3>
-        <p>This example adds a dropdown menu for the "Page 1" button in the navigation bar.</p>
-    </div>
-<!-- jQuery -->
-<!--<script src="//code.jquery.com/jquery.js"></script>-->
-<!-- Bootstrap JavaScript -->
-<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>-->
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="Hello World"></script>
+<div class="container-fluid">
+    <h3>Navbar With Dropdown</h3>
+    <p>This example adds a dropdown menu for the "Page 1" button in the navigation bar.</p>
 </div>
+
 
 <!-- Footer -->
 <section id="footer">
-    <div class="container">
+    <div class="container-fluid" style="background-color: #e84341">
         <div class="row text-center text-xs-center text-sm-left text-md-left">
             <div class="col-xs-12 col-sm-4 col-md-4">
                 <h5>Quick links</h5>
