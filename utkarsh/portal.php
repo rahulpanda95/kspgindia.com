ac23a3a95a139d9f9cf01d21e0854d74f1b18538
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-01-19 12:31:46 +0530

    EDIT HISTORY 9

--- utkarsh/portal.php
+++ utkarsh/portal.php
@@ -93,11 +93,11 @@ if (!isset($_SESSION["email"])) {
     <div class="container-fluid">
         <div class="row text-center text-xs-center text-sm-left text-md-left">
             <div class="col-xs-12 col-sm-4 col-md-4">
-                <h5>Quick links</h5>
+                <h5>My Portal</h5>
                 <ul class="list-unstyled quick-links">
-                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
-                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Information System</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Attendance System</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Apply for Leave</a></li>
                     <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
                     <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Videos</a></li>
                 </ul>
